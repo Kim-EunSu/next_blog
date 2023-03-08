0dcbc2478279155e0a69a68c4e9b3a6cd15077c2
Kim-EunSu <kes73640@gmail.com>
2023-03-08 15:41:10 +0900

    post데이터 렌더링

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -2,10 +2,12 @@ import PostGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
 
 function FeaturedPosts(props) {
+  const { posts } = props;
+
   return (
     <section className={classes.latest}>
       <h2>Featured Posts</h2>
-      <PostGrid posts={props.posts} />
+      <PostGrid posts={posts} />
     </section>
   );
 }
